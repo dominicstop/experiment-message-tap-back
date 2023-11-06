2ed06021894b1d231fb6bf231f389160a02b5d60
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-07 04:38:00 +0800

    💫 Update: `Experiment01ViewController`

--- experiment-message-tap-back/Screens/Experiment01ViewController.swift
+++ experiment-message-tap-back/Screens/Experiment01ViewController.swift
@@ -7,9 +7,101 @@
 
 import UIKit
 
+
 class Experiment01ViewController: UIViewController {
+
   override func viewDidLoad() {
-    super.viewDidLoad();
+    super.viewDidLoad()
     self.view.backgroundColor = .white;
+    
+    let stackView: UIStackView = {
+      let stack = UIStackView();
+      
+      stack.axis = .vertical;
+      stack.distribution = .fillProportionally;
+      stack.alignment = .center;
+      stack.spacing = 20;
+                
+      return stack;
+    }();
+    
+    let itemCount = 30;
+    let itemCountMid = Int(itemCount / 2);
+    
+    for index in 0 ..< itemCountMid {
+      let label = UILabel();
+      
+      label.text = "\(index)";
+      label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5);
+      label.alpha = 0.3;
+      label.font = .boldSystemFont(ofSize: 22);
+
+      stackView.addArrangedSubview(label);
+    };
+    
+    let box: UIView = {
+      let view = UIView();
+      
+      view.backgroundColor = .red;
+      view.layer.cornerRadius = 10;
+
+      return view;
+    }();
+    
+    box.translatesAutoresizingMaskIntoConstraints = false;
+    stackView.addArrangedSubview(box);
+    
+    NSLayoutConstraint.activate([
+      box.widthAnchor.constraint(equalToConstant: 200),
+      box.heightAnchor.constraint(equalToConstant: 100),
+    ]);
+    
+    for index in itemCountMid...itemCount {
+      let label = UILabel();
+      
+      label.text = "\(index)";
+      label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5);
+      label.alpha = 0.3;
+      label.font = .boldSystemFont(ofSize: 22);
+
+      stackView.addArrangedSubview(label);
+    };
+    
+    let scrollView: UIScrollView = {
+      let scrollView = UIScrollView();
+      
+      scrollView.showsHorizontalScrollIndicator = false;
+      scrollView.showsVerticalScrollIndicator = true;
+      return scrollView
+    }();
+    
+    stackView.translatesAutoresizingMaskIntoConstraints = false;
+    scrollView.addSubview(stackView);
+    
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(
+        equalTo: scrollView.topAnchor,
+        constant: 40
+      ),
+      
+      stackView.bottomAnchor.constraint(
+        equalTo: scrollView.bottomAnchor,
+        constant: -100
+      ),
+      
+      stackView.centerXAnchor.constraint(
+        equalTo: scrollView.centerXAnchor
+      ),
+    ]);
+    
+    scrollView.translatesAutoresizingMaskIntoConstraints = false;
+    self.view.addSubview(scrollView);
+    
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor     .constraint(equalTo: self.view.topAnchor     ),
+      scrollView.bottomAnchor  .constraint(equalTo: self.view.bottomAnchor  ),
+      scrollView.leadingAnchor .constraint(equalTo: self.view.leadingAnchor ),
+      scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+    ]);
   };
 };
